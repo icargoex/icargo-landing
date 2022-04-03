27267dcf6b997890925ebb1f01d03fab68fcc61e
Kenneth Chen <efeichen@gmail.com>
2022-04-03 11:19:00 -0400

    Fix gatsby-plugin-manifest config

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,15 +20,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `iCargoEx marketing website`,
+        short_name: `iCargoEx`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#FFC978`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo-square.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-emotion`,
